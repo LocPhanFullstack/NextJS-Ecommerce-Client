907f534acf4433b3d77da41ddcfb8b620fcd582d
LocPhanFullstack <phananhloc03102001@gmail.com>
2023-07-31 19:24:05 +0700

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">Home</div>
+        </Container>
+    );
+};
+
+export default HomePage;
